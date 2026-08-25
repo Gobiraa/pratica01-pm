5793286e89afd6cc35e220087e0c50d2e6d2bb2c
João Paulo Gobira <164088812+Gobiraa@users.noreply.github.com>
2026-08-25 17:57:45 -0300

    add:classe Pessoa

---
+++ src/main/java/com/example/Pessoa.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Pessoa {
+    public String nome;
+    public String sobrenome;
+    public int idade;
+    public double altura;
+    public double peso;
+    public double imc;
+
+
+    
+}
+
