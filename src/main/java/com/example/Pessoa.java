64e2bd80dcd618de1e906e5b48de18ce6b772f77
João Paulo Gobira <164088812+Gobiraa@users.noreply.github.com>
2026-08-25 18:01:33 -0300

    add: construtor e get/set

--- src/main/java/com/example/Pessoa.java
+++ src/main/java/com/example/Pessoa.java
@@ -9,6 +9,56 @@ public class Pessoa {
     public double imc;
 
 
+
+    public Pessoa(String nome, String sobrenome, int idade, double altura, double peso, double imc) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+        this.imc = imc;
+    }
+
+
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getSobrenome() {
+        return sobrenome;
+    }
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public double getAltura() {
+        return altura;
+    }
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    public double getPeso() {
+        return peso;
+    }
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+    public double getImc() {
+        return imc;
+    }
+    public void setImc(double imc) {
+        this.imc = imc;
+    }
+
+
     
 }
 
